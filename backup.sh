9bd246eb9b6d82a4bb420395ca0c66795e162651
Jj1910 <32131060+Jj1910@users.noreply.github.com>
2023-11-08 17:31:06 -0500

    Create backup.sh

---
+++ backup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+docker run --rm --volumes-from Traefik -v $(pwd):/backup busybox tar cvfz /backup/backup-ssl-certs.tar /ssl-certs
+docker run --rm --volumes-from FreshRSS -v $(pwd):/backup busybox tar cvfz /backup/backup-freshrss.tar /var/www/FreshRSS/data
+docker run --rm --volumes-from Wordpress -v $(pwd):/backup busybox tar cvfz /backup/backup-wordpress-data.tar /var/www/html
+docker run --rm --volumes-from Database -v $(pwd):/backup busybox tar cvfz /backup/backup-wordpress-database.tar /var/lib/mysql
