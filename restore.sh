8ce7d689c689a8777e55ee91ed9bb75d3ad61909
Jj1910 <32131060+Jj1910@users.noreply.github.com>
2023-11-10 08:41:38 -0500

    Update restore.sh

--- restore.sh
+++ restore.sh
@@ -3,3 +3,4 @@ docker run --rm --volumes-from Traefik -v $(pwd):/backup busybox sh -c "cd /ssl-
 docker run --rm --volumes-from FreshRSS -v $(pwd):/backup busybox sh -c "cd /var/www/FreshRSS/data && tar xvf /backup/backup-freshrss.tar --strip 1"
 docker run --rm --volumes-from Wordpress -v $(pwd):/backup busybox sh -c "cd /var/www/html && tar xvf /backup/backup-wordpress-data.tar --strip 1"
 docker run --rm --volumes-from Database -v $(pwd):/backup busybox sh -c "cd /var/lib/mysql && tar xvf /backup/backup-wordpress-database.tar --strip 1"
+docker run --rm --volumes-from Heimdall -v $(pwd):/backup busybox sh -c "cd /config && tar xvf /backup/backup-heimdall.tar --strip 1"
