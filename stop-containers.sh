dce8409d4234acdb0253cb549270154eef6f0881
Jj1910 <32131060+Jj1910@users.noreply.github.com>
2023-11-10 11:35:44 -0500

    Update stop-containers.sh

--- stop-containers.sh
+++ stop-containers.sh
@@ -4,3 +4,4 @@ docker compose -f Heimdall/docker-compose.yml down
 docker compose -f Nginx/docker-compose.yml down
 docker compose -f RSS-Server/docker-compose.yml down
 docker compose -f Wordpress/docker-compose.yml down
+#docker compose -f Portainer/docker-compose.yml down
