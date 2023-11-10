2969b3c4c154575246ed1fb9db035f20ff1b4d25
Jj1910 <32131060+Jj1910@users.noreply.github.com>
2023-11-10 11:35:26 -0500

    Update start-containers.sh

--- start-containers.sh
+++ start-containers.sh
@@ -4,3 +4,4 @@ docker compose -f Nginx/docker-compose.yml up -d
 docker compose -f RSS-Server/docker-compose.yml up -d
 docker compose -f Heimdall/docker-compose.yml up -d
 docker compose -f Wordpress/docker-compose.yml up -d
+#docker compose -f Portainer/docker-compose.yml up -d
