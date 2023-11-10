f180dcc22233886e86e9013303495bb24a49d093
Jj1910 <32131060+Jj1910@users.noreply.github.com>
2023-11-10 08:41:21 -0500

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -3,3 +3,4 @@ docker run --rm --volumes-from Traefik -v $(pwd):/backup busybox tar cvfz /backu
 docker run --rm --volumes-from FreshRSS -v $(pwd):/backup busybox tar cvfz /backup/backup-freshrss.tar /var/www/FreshRSS/data
 docker run --rm --volumes-from Wordpress -v $(pwd):/backup busybox tar cvfz /backup/backup-wordpress-data.tar /var/www/html
 docker run --rm --volumes-from Database -v $(pwd):/backup busybox tar cvfz /backup/backup-wordpress-database.tar /var/lib/mysql
+docker run --rm --volumes-from Heimdall -v $(pwd):/backup busybox tar cvfz /backup/backup-heimdall.tar /config
